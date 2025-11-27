aed08eaad6c8eb8b8e23ba975e33fff4e1ed211b
Nigel Patching <nigel.patching@york.ac.uk>
2025-11-27 22:31:09 +0000

    update tests

--- src.test.ts
+++ src.test.ts
@@ -11,4 +11,8 @@ describe('isEven', () => {
   it('returns true for even numbers', () => {
     expect(isEven(4)).toBe(true)
   })
+
+  it('returns false for odd numbers', () => {
+    expect(isEven(3)).toBe(false)
+  })
 })
